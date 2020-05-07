01c329550455a4046db7d99819e284de20e4eae5
Christopher Hunt <christopher.hunt@loadlock.co.uk>
2020-05-07 11:05:20 +0100

    Added scripts directory and test-everying bash script

---
+++ scripts/run_all_tests.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./bin/Regolith_test_vector
+./bin/Regolith_test_collision
+./bin/Regolith_test_exception
+./bin/Regolith_test_game_data
+./bin/Regolith_test_named_vector
+./bin/Regolith_test_integration
+
