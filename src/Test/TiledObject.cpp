30e58b536ca71316275709615c4bd50e1b9de805
Christopher Hunt <christopher.hunt@loadlock.co.uk>
2021-01-23 20:50:48 +0000

    Still working on the tiling approach. Might need to make the collision classes more complex.

---
+++ src/Test/TiledObject.cpp
@@ -0,0 +1,80 @@
+
+#include "Regolith/Test/TiledObject.h"
+#include "Regolith/Handlers/ContextGroup.h"
+#include "Regolith/Utilities/JsonValidation.h"
+
+
+namespace Regolith
+{
+
+  TiledObject::TiledObject() :
+    PhysicalObject(),
+    DrawableObject(),
+    _texture(),
+    _collision()
+  {
+  }
+
+
+  TiledObject::TiledObject( const TiledObject& other ) :
+    PhysicalObject( other ),
+    DrawableObject( other ),
+    _texture( other._texture ),
+    _collision( other._collision )
+  {
+  }
+
+
+  TiledObject::~TiledObject()
+  {
+  }
+
+
+  void TiledObject::configure( Json::Value& json_data, ContextGroup& cg )
+  {
+    // Configure the parent class first
+    PhysicalObject::configure( json_data, cg);
+
+    // Texture details
+    if ( validateJson( json_data, "texture", JsonType::OBJECT ) )
+    {
+      _texture.configure( json_data["texture"], cg.getDataHandler() );
+    }
+    
+    // Hitbox details
+    if ( validateJson( json_data, "collision", JsonType::OBJECT ) )
+    {
+      _collision.configure( json_data["collision"] );
+    }
+  }
+
+
+  void TiledObject::reset()
+  {
+    // Reset the parent object
+    PhysicalObject::reset();
+  }
+
+
+  Texture& TiledObject::getTexture()
+  {
+    return _texture;
+  }
+
+
+  Collision& TiledObject::getCollision()
+  {
+    return _collision;
+  }
+
+
+  void TiledObject::onCollision( Contact& contact, CollidableObject* /*other_object*/)
+  {
+    DEBUG_STREAM << "PhysicalObject::onCollision : " << contact.overlap << " | " << contact.impulse;
+    this->move( contact.inertiaRatio * contact.overlap * contact.normal );
+    this->kick( contact.impulse );
+    DEBUG_STREAM << "PhysicalObject::onCollision : Position : " << this->getPosition() << ", Vel : " << this->getVelocity();
+  }
+
+}
+
